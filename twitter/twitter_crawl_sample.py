5016c757c3c226fe224990add1dee1c56760c4f2
hykwtakumin <hykwtakumin@gmail.com>
2017-01-16 13:59:42 +0900

    tweepyのサンプル集を追加

--- twitter/twitter_crawl_sample.py
+++ twitter/twitter_crawl_sample.py
@@ -4,10 +4,10 @@ import sys
 import tweepy
 import codecs
 
-consumer_key        = ''
-consumer_secret     = ''
-access_token        = ''
-access_token_secret = ''
+consumer_key        = 'MjFWBvCRciIiSmroC2m2Ox1eR'    #右の4つはAPIの操作に必要なコードです(本当は直書きはマズイ)
+consumer_secret     = 'JwRC9Hj2OhhjPaKCwm2q5CY8hu32krIzzvXO3qgwkkv5jU7xNG'
+access_token        = '800556259506171904-DkUFHNBdBB9oiaNd3e0ZEB2WhHmPsF5'
+access_token_secret = 'vinXJMw8C7lVKU94MYM4zHCgXnj8YeUwlTDxwPlzQKrsd'
 
 # Twitter OAuth
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -56,4 +56,3 @@ if oldest != -1:
                 sys.exit()
 
 fp.close()
-
